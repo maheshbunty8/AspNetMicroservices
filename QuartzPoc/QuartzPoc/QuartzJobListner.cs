b45716bb47d95ad70e9a85ee5ef49088ecaeca3e
maheshbunty8 <mahesh.bunty@gmail.com>
2023-04-20 13:28:58 +0530

    updated code

--- QuartzPoc/QuartzPoc/QuartzJobListner.cs
+++ QuartzPoc/QuartzPoc/QuartzJobListner.cs
@@ -14,11 +14,33 @@ namespace QuartzPoc
         public async Task JobToBeExecuted(IJobExecutionContext context, CancellationToken cancellationToken = default)
         {
             await Console.Out.WriteLineAsync($"Job to be Executed :  {context.JobDetail.Key}");
+
+            bool isDataAvailable = CheckIfDataIsAvailable();
+
+            // If data is available, pause the job
+            if (isDataAvailable)
+            {
+                var scheduler = context.Scheduler;
+                var jobDetail = context.JobDetail;
+                var jobKey = jobDetail.Key;
+
+                scheduler.PauseJob(jobKey);
+
+                Console.WriteLine($"Job {jobKey} has been paused as data is available.");
+            }
+
         }
 
         public async Task JobWasExecuted(IJobExecutionContext context, JobExecutionException? jobException, CancellationToken cancellationToken = default)
         {
             await Console.Out.WriteLineAsync($"Job Executed :  {context.JobDetail.Key}");
+        }
+
+        private bool CheckIfDataIsAvailable()
+        {
+            // Check if data is available and return true or false
+            // Replace this with your own data availability check
+            return true;
         }
     }
 }
