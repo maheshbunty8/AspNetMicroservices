4768ce129b6a84d2861812fe1bd4a3f3060e2999
maheshbunty8 <mahesh.bunty@gmail.com>
2023-04-20 13:47:09 +0530

    updated for pause and resume

--- QuartzPoc/QuartzPoc/HelloJob.cs
+++ QuartzPoc/QuartzPoc/HelloJob.cs
@@ -6,9 +6,37 @@ namespace QuartzPoc
     {
         public Task Execute(IJobExecutionContext context)
         {
-            Console.WriteLine($"In Hello Job");
+            var jobKey = context.JobDetail.Key;
+            var scheduler = context.Scheduler;
 
-            return Task.CompletedTask;
+            // Get the current date and time
+            var currentTime = DateTime.Now;
+
+            // Set the pause time to the end of the current day
+            var pauseTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 23, 59, 59);
+
+            // Pause the job
+            await scheduler.PauseJob(jobKey);
+
+            // Schedule the job to resume after the pause time
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity("ResumeTrigger", "YourJobGroup")
+                .StartAt(pauseTime.AddSeconds(1))
+                .Build();
+
+            try
+            {
+                // Do the job processing
+                // ...
+
+                // Resume the job
+                await scheduler.ResumeJob(jobKey);
+            }
+            catch (JobExecutionException)
+            {
+                // If the job execution fails, reschedule the job to resume after the pause time
+                await scheduler.ScheduleJob(trigger);
+            }
         }
     }
 }
